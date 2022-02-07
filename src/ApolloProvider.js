63e6be44a8ba1a962758700df4fdaa3f224e07af
Humberto Ham <hamduranhumberto@gmail.com>
2022-02-07 11:51:31 -0600

    proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 } from "@apollo/client";
 import { setContext } from "apollo-link-context";
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000",
+  uri: "https://sleepy-refuge-94868.herokuapp.com/",
 });
 const authLink = setContext(() => {
   const token = localStorage.getItem("jwtToken");
